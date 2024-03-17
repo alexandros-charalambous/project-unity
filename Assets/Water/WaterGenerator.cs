94d6fe5323d18b6876044a860f0db91f5732fb1b
Alexandros Charalambous <alexantros.16@hotmail.com>
2024-03-17 11:00:21 +0200

    Endless water generation

--- Assets/Water/WaterGenerator.cs
+++ Assets/Water/WaterGenerator.cs
@@ -4,84 +4,144 @@ using UnityEngine;
 
 public class WaterGenerator : MonoBehaviour
 {
-    const float playerMoveThresholdToUpdate = 25f;
-    const float sqrPlayerMoveThreshholdToUpdate = playerMoveThresholdToUpdate * playerMoveThresholdToUpdate;
+	public const float maxViewDistance = 7200f;
+	public Transform player;
 
+	public static Vector2 playerPosition;
+	public Material material;
+	int chunkSize;
+	int chunksVisibleInViewDst;
 
-    public MeshSettings meshSettings;
-    public Transform player;
-    public Material waterMaterial;
+	Dictionary<Vector2, WaterChunk> waterChunkDictionary = new Dictionary<Vector2, WaterChunk>();
+	List<WaterChunk> waterChunksVisibleLastUpdate = new List<WaterChunk>();
 
-    Vector2 playerPosition;
-    Vector2 playerPositionOld;
+	void Start()
+	{
+		chunkSize = 720;
+		chunksVisibleInViewDst = Mathf.RoundToInt(maxViewDistance / chunkSize);
+	}
 
-    float meshWorldSize;
-    int chunksVisibleInViewDistance;
+	void Update()
+	{
+		playerPosition = new Vector2(player.position.x, player.position.z);
+		UpdateVisibleChunks();
+	}
 
-    Dictionary<Vector2, WaterChunk> waterChunkDictionary = new Dictionary<Vector2, WaterChunk>();
-    List<WaterChunk> visibleWaterChunks = new List<WaterChunk>();
+	void UpdateVisibleChunks()
+	{
 
-    void Start()
-    {        
-        float maxViewDistance = 7200f;
-        meshWorldSize = meshSettings.meshWorldSize;
-        chunksVisibleInViewDistance = Mathf.RoundToInt(maxViewDistance / meshWorldSize);
-        
-        UpdateVisibleChunks();
-    }
+		for (int i = 0; i < waterChunksVisibleLastUpdate.Count; i++)
+		{
+			waterChunksVisibleLastUpdate[i].SetVisible(false);
+		}
+		waterChunksVisibleLastUpdate.Clear();
 
-    void Update()
-    {
-        playerPosition = new Vector2(player.position.x, player.position.z);
+		int currentChunkCoordX = Mathf.RoundToInt(playerPosition.x / chunkSize);
+		int currentChunkCoordY = Mathf.RoundToInt(playerPosition.y / chunkSize);
 
-        if((playerPositionOld - playerPosition).sqrMagnitude > sqrPlayerMoveThreshholdToUpdate)
-        {   
-            playerPositionOld = playerPosition;
-            UpdateVisibleChunks();
-        }
-    }
+		for (int yOffset = -chunksVisibleInViewDst; yOffset <= chunksVisibleInViewDst; yOffset++)
+		{
+			for (int xOffset = -chunksVisibleInViewDst; xOffset <= chunksVisibleInViewDst; xOffset++)
+			{
+				Vector2 viewedChunkCoord = new Vector2(currentChunkCoordX + xOffset, currentChunkCoordY + yOffset);
 
-    void UpdateVisibleChunks()
-    {
-        HashSet<Vector2> alreadyUpdatedChunkCoords = new HashSet<Vector2>();
-        for (int i = visibleWaterChunks.Count - 1; i >= 0; i--)
-        {
-            alreadyUpdatedChunkCoords.Add(visibleWaterChunks[i].coord);
-            visibleWaterChunks[i].UpdateWaterChunk();
-        }
+				if (waterChunkDictionary.ContainsKey(viewedChunkCoord))
+				{
+					waterChunkDictionary[viewedChunkCoord].UpdateWaterChunk();
+					if (waterChunkDictionary[viewedChunkCoord].IsVisible())
+					{
+						waterChunksVisibleLastUpdate.Add(waterChunkDictionary[viewedChunkCoord]);
+					}
+				}
+				else
+				{
+					WaterChunk newChunk = new WaterChunk(viewedChunkCoord, chunkSize, transform, material);
+					waterChunkDictionary.Add(viewedChunkCoord, newChunk);
+					newChunk.Load();
+				}
 
-        int currentChunkCoordX = Mathf.RoundToInt(playerPosition.x / meshWorldSize);
-        int currentChunkCoordY = Mathf.RoundToInt(playerPosition.y / meshWorldSize);
+			}
+		}
+	}
 
-        for (int yOffset = -chunksVisibleInViewDistance; yOffset <= chunksVisibleInViewDistance; yOffset++) { 
-            for (int xOffset = -chunksVisibleInViewDistance; xOffset <= chunksVisibleInViewDistance; xOffset++) {
-                Vector2 viewedChunkCoord = new Vector2(currentChunkCoordX + xOffset, currentChunkCoordY + yOffset);
-                if (!alreadyUpdatedChunkCoords.Contains(viewedChunkCoord))
-                {
-                    if (waterChunkDictionary.ContainsKey(viewedChunkCoord))
-                    {
-                        waterChunkDictionary[viewedChunkCoord].UpdateWaterChunk();
-                    }
-                    else
-                    {
-                        WaterChunk newChunk = new WaterChunk(viewedChunkCoord, meshSettings, transform, player, waterMaterial);
-                        waterChunkDictionary.Add(viewedChunkCoord, newChunk);
-                        newChunk.onVisibilityChange += OnWaterChunkVisibilityChanged;
-                    }                    
-                }
-            }
-        }
-    }    
+	[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+	public class WaterChunk
+	{
+		GameObject meshObject;
+		MeshCollider meshCollider;
+		MeshRenderer meshRenderer;
+		Vector2 position;
+		Bounds bounds;
 
-    void OnWaterChunkVisibilityChanged(WaterChunk chunk, bool isVisible)
-    {
-        if (isVisible)
-        {
-            visibleWaterChunks.Add(chunk);
-        }
-        else
-        {
-            visibleWaterChunks.Remove(chunk);
-        }
-    }
+		bool planeReceived;
+		
+    	public event System.Action<WaterChunk, bool> onVisibilityChange;
+
+		public WaterChunk(Vector2 coord, int size, Transform parent, Material material)
+		{
+			position = coord * size;
+			bounds = new Bounds(position, Vector2.one * size);
+			Vector3 positionV3 = new Vector3(position.x, 200, position.y);
+
+			meshObject = GameObject.CreatePrimitive(PrimitiveType.Plane);
+			meshObject.name = "Water Chunk";
+			
+			meshCollider = meshObject.GetComponent<MeshCollider>();
+			meshRenderer = meshObject.GetComponent<MeshRenderer>();
+
+			meshRenderer.material = material;
+
+			meshCollider.convex = true;
+			meshCollider.isTrigger = true;
+			meshCollider.sharedMesh = null;
+
+			meshObject.transform.position = positionV3;
+			meshObject.transform.localScale = Vector3.one * size / 10f;
+			meshObject.transform.parent = parent;
+			meshObject.layer = LayerMask.NameToLayer("Water");
+
+			SetVisible(false);
+		}
+
+		public void Load()
+		{
+			ThreadedDataRequester.RequestData(() => meshObject, OnPlaneReceived);
+		}
+
+		void OnPlaneReceived(object planeObject)
+		{
+			this.meshObject = (GameObject)planeObject;
+			planeReceived = true;
+
+			UpdateWaterChunk();
+		}
+
+		public void UpdateWaterChunk()
+		{
+			if (planeReceived)
+			{
+				float playerDstFromNearestEdge = Mathf.Sqrt(bounds.SqrDistance(playerPosition));
+				bool wasVisible = IsVisible();
+				bool visible = playerDstFromNearestEdge <= maxViewDistance;			
+				if (wasVisible != visible)
+				{
+					SetVisible(visible);    
+					if (onVisibilityChange != null)
+					{
+						onVisibilityChange(this, visible);                       
+					}
+				}
+			}
+		}
+
+		public void SetVisible(bool visible)
+		{
+			meshObject.SetActive(visible);
+		}
+
+		public bool IsVisible()
+		{
+			return meshObject.activeSelf;
+		}
+	}
 }
